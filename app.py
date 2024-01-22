72396ada2e0978815b072be8fade527bac442b40
Wildeswiesel <152853691+Wildeswiesel@users.noreply.github.com>
2024-01-22 12:57:06 +0100

    kommentiert + popup css

--- app.py
+++ app.py
@@ -4,18 +4,18 @@ app = Flask(__name__)
 
 gespeicherte_daten = []
 
-@app.route('/')
+@app.route('/') #rendert/ruft startseite auf
 def startseite():
     return render_template('index.html', daten=gespeicherte_daten)
 
 @app.route('/popup', methods=['GET', 'POST'])
 def popup():
     if request.method == 'POST':
-        eingabe_daten = request.form['daten']
-        gespeicherte_daten.append(eingabe_daten)
-        return render_template('popup.html', eingabe_daten=eingabe_daten)
-    return render_template('popup.html')
+        eingabe_daten = request.form['daten']# Extrahiere Daten aus dem Formularfeld 'daten' der POST-Anfrage
+        gespeicherte_daten.append(eingabe_daten)# Füge die empfangenen Daten zur Liste 'gespeicherte_daten' hinzu
+        return render_template('popup.html', eingabe_daten=eingabe_daten)# Rendere das HTML-Template 'popup.html' und übergebe 'eingabe_daten'
+    return render_template('popup.html')# Wenn keine POST-Anfrage vorliegt, rendere einfach das HTML-Template 'popup.html'
 
-print(gespeicherte_daten)
+
 if __name__ == '__main__':
     app.run(debug=True)
