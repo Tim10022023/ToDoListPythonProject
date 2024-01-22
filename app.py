f97d6680d0272b6b824f65c846c9125301900153
Wildeswiesel <152853691+Wildeswiesel@users.noreply.github.com>
2024-01-22 12:42:21 +0100

    Neues popup fenster wird geöffnet wenn der knopf gedrückt wird und daten können eingegeben werden

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+gespeicherte_daten = []
 
 @app.route('/')
 def startseite():
-    return render_template("index.html")
+    return render_template('index.html', daten=gespeicherte_daten)
 
-if __name__ == "__main__":
+@app.route('/popup', methods=['GET', 'POST'])
+def popup():
+    if request.method == 'POST':
+        eingabe_daten = request.form['daten']
+        gespeicherte_daten.append(eingabe_daten)
+        return render_template('popup.html', eingabe_daten=eingabe_daten)
+    return render_template('popup.html')
+
+print(gespeicherte_daten)
+if __name__ == '__main__':
     app.run(debug=True)
