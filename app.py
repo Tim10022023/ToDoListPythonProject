6acc2f03c726ab67e4fb4431a81589f22dcb8ae7
Tim Leute <tim.leute@wuerth-industrie.com>
2024-01-28 11:44:40 +0100

    .

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, request
+from flask import render_template
+from flask import redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
+class Task(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text)
+    done = db.Column(db.Boolean, default = False)
+
+    def __init__(self, content):
 
 gespeicherte_daten = []
 gespeichertes_datum = []
