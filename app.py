bb5a2bc84c81c6d170dd63b6a4376fd79c6a76ae
Wildeswiesel <152853691+Wildeswiesel@users.noreply.github.com>
2024-01-30 14:50:27 +0100

    kp

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def update_task_status(task_id):
 
 
 if __name__ == '__main__':
-    #app.run(host='0.0.0.0', port=5000) #wird benötigt damit man vom lokalen Netz zugreifen kann
+    app.run(host='0.0.0.0', port=5000) #wird benötigt damit man vom lokalen Netz zugreifen kann
     with app.app_context():
         db.create_all()
     app.run(debug=True)
